bd627da0f3461b9301683cb9b8acdcc0f463458b
cl4ss1fi3d <cl4ss1fi3d.tutamail.com>
2026-03-18 16:11:01 +0100

    updated installer

--- installer.sh
+++ installer.sh
@@ -8,3 +8,13 @@ fi
 apt install nmap
 apt install figlet
 apt install git
+
+clear 
+figlet m4pper install
+
+mkdir /bin/m4pper/
+
+curl -o /bin/m4pper/m4pper https://raw.githubusercontent.com/cl4ss1fi3d/m4pper/refs/heads/main/m4pper
+curl -o /bin/m4pper/m4pper-src https://raw.githubusercontent.com/cl4ss1fi3d/m4pper/refs/heads/main/m4pper-src
+
+echo 'PATH="$PATH:$HOME/bin/m4pper"' >> "$HOME/.bashrc"
