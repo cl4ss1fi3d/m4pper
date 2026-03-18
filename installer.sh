f7720eea1d83e54d7d43d46dfc907cca62d0fb17
cl4ss1fi3d <cl4ss1fi3d.tutamail.com>
2026-03-18 17:01:36 +0100

    fixed installer

--- installer.sh
+++ installer.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-if [ "$(whoami)" != "root" ]; then
-        echo "Ejecutando como administrador..."
-        exec sudo "$0" "$@"
+if [ "$(whoami)" == "root" ]; then
+        echo "[!] Por favor, no ejecutes como root."
+        exit 1
 fi
 
-apt install nmap
-apt install figlet
-apt install git
+sudo apt install nmap
+sudo apt install git
+sudo apt install figlet
 
 clear 
 figlet m4pper install
 
-mkdir /bin/m4pper/
+cd /bin
 
-curl -o /bin/m4pper/m4pper https://raw.githubusercontent.com/cl4ss1fi3d/m4pper/refs/heads/main/m4pper
-curl -o /bin/m4pper/m4pper-src https://raw.githubusercontent.com/cl4ss1fi3d/m4pper/refs/heads/main/m4pper-src
-
-echo 'PATH="$PATH:$HOME/bin/m4pper"' >> "$HOME/.bashrc"
+sudo curl -O https://raw.githubusercontent.com/cl4ss1fi3d/m4pper/refs/heads/main/m4pper
+sudo chmod +x m4pper
+sudo curl -O https://raw.githubusercontent.com/cl4ss1fi3d/m4pper/refs/heads/main/m4pper-src
+cd - &> /dev/null
