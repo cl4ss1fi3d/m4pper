63b2465cd5dd199f09dc9d21b661c056e8abf4fa
cl4ss1fi3d <cl4ss1fi3d.tutamail.com>
2026-03-18 14:01:38 +0100

    ashdb

--- src.sh
+++ src.sh
@@ -2,6 +2,7 @@
 
 RED="\e[31m"
 BLUE="\e[34m"
+CYAN="\e[36m"
 GREEN="\e[32m"
 EC="\e[0m"
 
@@ -24,11 +25,35 @@ check_install() {
 
 full_scan() {
     read -p "IP [Default: 192.168.1.0/24]: " ip
-    read -p "Guardar en archivo? [Y/n]: " save
-    if [[ "${save,,}" == "y" || "$save" == "" ]]; then
-        echo ok
+    if [ -z "$ip" ]; then
+        ip="192.168.1.0/24"
+    elif [[ ! $ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}(/([0-9]|[1-2][0-9]|3[0-2]))?$ ]]; then
+        menu "error"
     fi
 
+    read -p "Guardar en archivo? [Y/n]: " save
+    if [[ "${save,,}" == "y" || -z "$save" ]]; then
+        read -p "Nombre del archivo [Default: result]: " file
+        nmap -vv -sV -O $ip -oG "$file".gnmap
+        echo -e "${GREEN}"
+        read -p "[#] Leer archivo? [y/N]: " read
+        if [[ -z "$read" || "${read,,}" == "n" ]]; then
+            echo -e "[*] Puedes leerlo con 'cat "$file".gnmap'"
+            echo -e "${EC}"
+            sleep 3
+            clear
+            exit 0
+        elif [[ "${read,,}" == "y" ]]; then
+            clear
+            cat "$file".gnmap
+            exit 0
+        else
+            clear
+            exit 0
+        fi
+    fi
+
+    nmap -vv -sV -O $ip
 }
 
 port_scan() {
@@ -47,22 +72,25 @@ menu() {
     echo ""
 
     # Mostrar mensaje de error si se pasa como argumento
-    if [ -n "$1" ]; then
+    if [ "$1" == "error" ]; then
         echo -e "${RED}[!] Opción no válida.${EC}"
         echo ""
     fi
 
     echo -e "${GREEN}[1] Escanear toda la red."
     echo -e "[2] Escanear toda la red en búsqueda de puertos."
-    echo -e "[3] Escanear vulnerabilidades en IP.${EC}"
-    echo ""
+    echo -e "[3] Escanear vulnerabilidades en IP."
+    echo -e "[Q] Cerrar el programa."
+    echo -e "${CYAN}"
 
-    read -p "[#] Opción: " input
+    read -p "[#] Opción: " {input,,}
+    echo -e "${EC}"
 
     case "$input" in
         1) full_scan;;
         2) port_scan;;
         3) vuln_scan;;
+        q) clear; exit 0;;
         *) menu "error";;  # pasamos "error" para mostrar el mensaje
     esac
 }
